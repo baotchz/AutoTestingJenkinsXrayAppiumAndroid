e4c426c873e6e24c00e47fe4e0bede682128f821
NECSV-HCM\baotchz <baotchz@nec.vn>
2023-09-21 23:46:37 +0700

    add project test

---
+++ app/src/test/java/com/example/appiumapplication2/AndroidTest.java
@@ -0,0 +1,51 @@
+package com.example.appiumapplication2;
+
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.MobilePlatform;
+
+public class AndroidTest {
+
+    @Test
+    public void androidLaunchTest() throws MalformedURLException, InterruptedException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName(MobilePlatform.ANDROID);
+        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
+        options.setDeviceName("emulator-5554");
+
+        System.out.println(System.getProperty("user.dir")+"/resource_apk/Android-MyDemoAppRN.1.3.0.build-244.apk");
+        options.setApp(System.getProperty("user.dir")+"/resource_apk/Android-MyDemoAppRN.1.3.0.build-244.apk");
+
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
+        Thread.sleep(4000);
+
+        //======testcase=====//
+        driver.findElement(AppiumBy.accessibilityId("open menu")).click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+            .until(e->e.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"menu item log in\"]")));
+
+        driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"menu item log in\"]"))
+                .click();
+         Thread.sleep(5000);
+        driver.findElement(AppiumBy.accessibilityId("Username input field")).sendKeys("hahahahah");
+        Thread.sleep(5000);
+        driver.quit();
+
+        //Assertion
+
+    }
+
+
+}
